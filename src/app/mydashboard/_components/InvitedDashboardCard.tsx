7463cd37a39fb71a3e532960d71d8de0b71f1728
chaemin58 <sulv0309@naver.com>
2026-07-02 02:07:15 +0900

    fix: invitionRow 컴포넌트 invitedDashboardCard로 수정

--- src/app/mydashboard/_components/InvitedDashboardCard.tsx
+++ src/app/mydashboard/_components/InvitedDashboardCard.tsx
@@ -3,37 +3,36 @@
 import { Button } from "@/components/Button";
 import { ProfileWrapper } from "@/components/profile/Profile";
 
-import { UserInfo } from "./InvitionContainer";
-
-export function InvitionRow({
-  title,
-  inviter,
-  id,
-  handleDismiss,
-  handleAccept,
-}: {
+interface InvitedDashboardCardProps {
   title: string;
-  inviter: UserInfo;
+  name: string;
   id: number;
-  handleDismiss: (id: number) => void;
-  handleAccept: (id: number) => void;
-}) {
+  onClickDismiss: (id: number) => void;
+  onClickAccept: (id: number) => void;
+}
+export function InvitedDashboardCard({
+  title,
+  name,
+  id,
+  onClickDismiss,
+  onClickAccept,
+}: InvitedDashboardCardProps) {
   return (
     <div className="flex flex-col justify-between gap-1 px-2.5 pt-4 pb-4.5 md:flex-row">
       <div className="w-75">{title}</div>
       <div className="flex items-center justify-between">
         <div className="w-50">
-          <ProfileWrapper name={inviter.nickname} />
+          <ProfileWrapper name={name} />
         </div>
         <div className="flex w-32.5 gap-3">
           <Button
-            size={"sm"}
-            colorType={"secondary"}
-            onClick={() => handleDismiss(id)}
+            size="sm"
+            colorType="secondary"
+            onClick={() => onClickDismiss(id)}
           >
             거절
           </Button>
-          <Button size={"sm"} onClick={() => handleAccept(id)}>
+          <Button size="sm" onClick={() => onClickAccept(id)}>
             수락
           </Button>
         </div>
