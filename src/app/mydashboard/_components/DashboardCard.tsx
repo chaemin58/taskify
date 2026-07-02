d3412c88893a9436581d71f3c761ed7e54e14de5
chaemin58 <sulv0309@naver.com>
2026-07-03 00:28:53 +0900

    fix: 경로 오타 수정

--- src/app/mydashboard/_components/DashboardCard.tsx
+++ src/app/mydashboard/_components/DashboardCard.tsx
@@ -1,9 +1,9 @@
 "use client";
 
+import Link from "next/link";
+
 import Colorchip from "@/assets/dashboard/ic-colorchips.svg";
 import RightArrow from "@/assets/mydashboard/ic_right_arrow.svg";
-
-import Link from "next/link";
 
 interface DashboardCardProps {
   title: string;
@@ -12,7 +12,7 @@ interface DashboardCardProps {
 export function DashboardCard({ title, dashboardId }: DashboardCardProps) {
   return (
     <Link
-      href={`/dashboard${dashboardId}`}
+      href={`/dashboard/${dashboardId}`}
       className="bg-black-700 relative flex h-20 w-full max-w-89 min-w-55 items-center justify-between rounded-3xl border border-gray-800 p-5"
     >
       <div className="flex items-center justify-center">
