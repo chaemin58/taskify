53431c65a0b95687c18621aed3da0a0d69bb4f65
chaemin58 <sulv0309@naver.com>
2026-07-15 15:26:15 +0900

    fix: ci 에러 해결

--- src/app/dashboard/[id]/_components/NoCard.tsx
+++ src/app/dashboard/[id]/_components/NoCard.tsx
@@ -1,4 +1,4 @@
-export function NoCard({ id }: { id: number }) {
+export function NoCard() {
   return (
     <div className="m-auto flex flex-col gap-8 pt-15">
       <div className="text-black-500 text-md m-auto font-bold">
